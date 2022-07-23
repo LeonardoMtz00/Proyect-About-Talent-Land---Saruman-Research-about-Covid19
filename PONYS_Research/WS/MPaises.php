a1d61f941672f9c2bd10367683f3fbcfca512d01
AxelMVE <109844416+AxelMVE@users.noreply.github.com>
2022-07-23 05:11:39 -0500

    Update MPaises.php

--- PONYS_Research/WS/MPaises.php
+++ PONYS_Research/WS/MPaises.php
@@ -1,15 +1,13 @@
 <?php
 
-//error_reporting(0); //status 0, no mostrar errores
 
-//ENDPOINT es punto de acceso a mi web service
 
-require 'DBManager.php'; //manejador de base de datos, que seria como mi "import"
+require 'DBManager.php'; 
 
 
 
-$manager = new \DBManager(); //creamos clase dinamicamente
-$json = $manager->showPaises(); //si no es nulo invocamos a find y mandamos id 
+$manager = new \DBManager(); 
+$json = $manager->showPaises(); 
 
 
 
